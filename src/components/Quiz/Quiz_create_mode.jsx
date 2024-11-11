ca35947dcf69c0e255c7d75432e5945b394d5115
allorak333 <allorak333@gmail.com>
2024-11-11 13:01:37 +0900

    [client] style : wrong css fixed
    font size fixed in mode select
    screan move button position moved

--- src/components/Quiz/Quiz_create_mode.jsx
+++ src/components/Quiz/Quiz_create_mode.jsx
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
-import "./CSS/Quiz_create_mode.css"
+import styles from "./CSS/Quiz_create_mode.module.css"
 
 const ModeSelection = ({ mode, handleModeSelect }) => {
-    return (
-      <div className="mode-selection">
-        <div
-          className={`mode-button mode-button-basic ${mode === 1 ? "selected" : ""}`}
-          onClick={() => handleModeSelect(1)}
-        >
-          <h2>기본</h2>
-          {/* <p></p> */}
-        </div>
-        <div
-          className={`mode-button mode-button-giant ${mode === 2 ? "selected" : ""}`}
-          onClick={() => handleModeSelect(2)}
-        >
-          <h2>AI 악기 분리</h2>
-          {/* <p></p> */}
-        </div>
+  return (
+    <div className={styles["mode-selection"]}>
+      <div
+        className={`${styles["mode-button"]} ${styles["mode-button-basic"]} ${mode === 1 ? styles["selected"] : ""}`}
+        onClick={() => handleModeSelect(1)}
+      >
+        <span className={styles["mode-name"]}>기본</span>
       </div>
-    );
-  };
+      <div
+        className={`${styles["mode-button"]} ${styles["mode-button-giant"]} ${mode === 2 ? styles["selected"] : ""}`}
+        onClick={() => handleModeSelect(2)}
+      >
+        <span className={styles["mode-name"]}>AI 악기 분리</span>
+      </div>
+    </div>
+  );
+};
 
 export default ModeSelection;
